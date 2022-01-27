42877a6917c932116d68ad4a0e2bda7afa9d352b
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-01-27 21:20:54 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -163,21 +163,7 @@ def start(update: Update, context: CallbackContext):    # sourcery no-metrics
                                 url="t.me/{}?startgroup=true".format(
                                     context.bot.username
                                 ),
-                            ),
-                        ],
-                        [
-                            InlineKeyboardButton(
-                                text=gs(chat.id, "support_chat_link_btn"),
-                                url='https://t.me/Decodesupport',
-                            ),
-                            InlineKeyboardButton(
-                                text=gs(chat.id, "updates_channel_link_btn"),
-                                url="https://t.me/Deecodebots",
-                            ),
-                        ],
-                    ]
-                ),
-            )
+                            )
 
             context.bot.answer_callback_query(query.id)
             return
